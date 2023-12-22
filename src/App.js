941fca485d4df7d0f2957f8a20f36c485d79b75e
zeriong <jaeryong95@gmail.com>
2023-12-22 22:53:58 +0900

    react: useEffect와 useState에 의한 렌더링 순서에 대한 복습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import {ReactRenderingOrder} from './list/ReactRenderingOrder';
+
 function App() {
   return (
     <div>
-      Hello World!
+        <h1>
+            Hello World!
+        </h1>
+        <ReactRenderingOrder/>
     </div>
   );
 }
---
+++ src/list/ReactRenderingOrder.js
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react';
+
+export const ReactRenderingOrder = () => {
+    console.log('a')
+
+    const [state, setState] = useState(false);
+    console.log('b')
+
+    useEffect(() => {
+        console.log('c')
+        setTimeout(() => {
+            setState(true);
+            console.log('d')
+        }, 1000)
+    }, []);
+
+    // 렌더링 순서는
+    // a b c d a b 이다.
+    // setState함수에는 render함수가 내포되어있어 컴포넌트를 재실행 시켜준다.
+    // ** (render 매서드가 내포 되어있다는 것을 잊지말 것)
+
+    return (
+        <div>
+            ReactRenderingOrder
+        </div>
+    )
+}
