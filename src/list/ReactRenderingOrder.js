79537cc1ef6b043b01284a55b47307c1df75c494
zeriong <jaeryong95@gmail.com>
2023-12-23 09:34:07 +0900

    react: useLayoutEffect 렌더링 순서 복습

--- src/list/ReactRenderingOrder.js
+++ src/list/ReactRenderingOrder.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useLayoutEffect, useState} from 'react';
 
 export const ReactRenderingOrder = () => {
     console.log('a')
@@ -18,6 +18,11 @@ export const ReactRenderingOrder = () => {
     // a b c d a b 이다.
     // setState함수에는 render함수가 내포되어있어 컴포넌트를 재실행 시켜준다.
     // ** (render 매서드가 내포 되어있다는 것을 잊지말 것)
+
+    // useLayoutEffect는 useEffect보다 먼저 실행됨 (paint이전에 layout단계에서 실행되기 떄문)
+    useLayoutEffect(() => {
+        console.log('A')
+    }, []);
 
     return (
         <div>
