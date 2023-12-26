86532ad7c686786d14906876f2faa912e3d9fd81
zeriong <jaeryong95@gmail.com>
2023-12-26 21:02:14 +0900

    react: 스프레드함수를 통한 깊은 복사

--- src/list/DeepCopy.js
+++ src/list/DeepCopy.js
@@ -1,8 +1,13 @@
 export const DeepCopy = () => {
     let obj = { str: 100, dex: 200, int: 1 }
-    let copyObj = obj;
+    // 완전히 별도의 오브젝트를 만들기 위해서는 스프레드함수(...)을 사용할 수 있다.
+    let copyObj = {...obj};
 
     obj.dex = 4444;
+    obj.str = 4444;
+    obj.int = 4444;
+
+    copyObj.dex = 9999;
 
     console.log(copyObj)
 
