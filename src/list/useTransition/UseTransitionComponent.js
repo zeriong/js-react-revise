b77275f41f95481fea5f34cf5451b11563c2e018
jaery <jaeryong95@gmail.com>
2024-07-24 20:29:38 +0900

    useTransition 예시 데이터를 생성하기 위해 faker.js 설치

--- src/list/useTransition/UseTransitionComponent.js
+++ src/list/useTransition/UseTransitionComponent.js
@@ -1,13 +1,24 @@
-import {useState} from "react";
+import {useState, useTransition} from "react";
 
 export default function UseTransitionComponent() {
+    const [isPending, startTransition] = useTransition();
     const [inputVal, setInputVal] = useState("");
+
+    const handleInputChange = (evt) => {
+        startTransition(() => {
+
+        });
+    }
+
     return (
         <div>
             <p>UseTransitionComponent</p>
             <div>
-                <input type="text" value={inputVal} onChange={(e) => setInputVal(e.target.value)} />
+                <input type="text" onChange={handleInputChange} />
             </div>
+            {isPending ?
+                <p style={{ fontSize: "30px", fontWeight: "bold" }}>Pending...</p>
+                : inputVal}
         </div>
     )
 }
