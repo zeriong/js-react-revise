ccb6f27d314706666b64535dbc5edaeea51bde14
jeryong <jaeryong95@gmail.com>
2024-07-25 14:51:25 +0900

    users mock data 에 mongodbObjectId 매서드를 할당한 id 추가

--- src/mock/users.js
+++ src/mock/users.js
@@ -4,6 +4,7 @@ const THRESHOLD = 10000;
 
 export const users = Array.from(Array(THRESHOLD), () => {
     return {
+        id: faker.database.mongodbObjectId(),
         name: faker.person.fullName(),
         avatar: faker.image.avatar(),
         background: faker.image.urlPicsumPhotos(),
